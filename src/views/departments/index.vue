03f225b0c300ceb044d5f4522665400a86bbe41c
nylhc <nylhc66@gmail.com>
2023-02-17 18:34:35 +0800

    封装单独的树操作栏组件

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,18 +1,72 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        组织架构
-      </h2>
+      <!-- 实现页面的基本布局 -->
+      <el-card class="tree-card">
+        <!-- 用了一个行列布局 -->
+        <!-- 缺少treeNode -->
+        <tree-tools
+          :tree-node="company"
+          :is-root="true"
+        />
+        <!--放置一个属性   这里的props和我们之前学习的父传子 的props没关系-->
+        <el-tree
+          :data="departs"
+          :props="defaultProps"
+          default-expand-all
+        >
+          <!-- 说明el-tree里面的这个内容 就是插槽内容 => 填坑内容  => 有多少个节点循环多少次 -->
+          <!-- scope-scope 是 tree组件传给每个节点的插槽的内容的数据 -->
+          <!-- 顺序一定是 执行slot-scope的赋值 才去执行 props的传值 -->
+          <tree-tools
+            slot-scope="{ data }"
+            :tree-node="data"
+          />
+        </el-tree>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
+import TreeTools from './components/tree-tools.vue'
 export default {
-
+  components: {
+    TreeTools
+  },
+  data () {
+    return {
+      company: {
+        name: '江苏传智播客教育科技股份有限公司',
+        manager: '负责人'
+      },
+      departs: [{
+        name: '总裁办',
+        manager: '曹操',
+        children: [{
+          name: '董事会',
+          manager: '曹丕'
+        }]
+      },
+      {
+        name: '行政部',
+        manager: '刘备'
+      },
+      {
+        name: '人事部',
+        manager: '孙权'
+      }],
+      defaultProps: {
+        label: 'name' // 表示 从这个属性显示内容
+      }
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped>
+.tree-card {
+  padding: 30px 140px;
+  font-size: 14px;
+}
 </style>
