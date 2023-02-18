98a82dc41c39eab5118652e7f7edea3471992c19
nylhc <nylhc66@gmail.com>
2023-02-18 20:08:06 +0800

    员工列表数据请求和分页加载

--- src/api/employees.js
+++ src/api/employees.js
@@ -1,10 +1,20 @@
 import request from '@/utils/request'
 
 /**
- *  获取员工的简单列表
+ * 获取员工的简单列表
  * **/
 export function getEmployeeSimple() {
   return request({
     url: '/sys/user/simple'
+  })
+}
+
+/**
+ * 获取员工的综合列表数据
+ * ***/
+export function getEmployeeList(params) {
+  return request({
+    url: '/sys/user',
+    params
   })
 }
