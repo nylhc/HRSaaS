610d7bf6f24991dad43687c6d1ae7a4889d71465
nylhc <nylhc66@gmail.com>
2023-02-25 19:20:11 +0800

    员工详情中应用上传组件

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -38,6 +38,20 @@
             prop="username"
           />
           <el-table-column
+            label="头像"
+            align="center"
+          >
+            <template slot-scope="{row}">
+              <img
+                slot="reference"
+                v-imageerror="require('@/assets/common/bigUserHeader.png')"
+                :src="row.staffPhoto "
+                style="border-radius: 50%; width: 100px; height: 100px; padding: 10px"
+                alt=""
+              >
+            </template>
+          </el-table-column>
+          <el-table-column
             label="工号"
             sortable=""
             prop="workNumber"
